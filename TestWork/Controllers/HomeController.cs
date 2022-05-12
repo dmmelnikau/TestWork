dd25ef5636b3389b231a6f62ac4a33485a45481c
okmij <okmij@DESKTOP-26N6QAU>
2022-05-13 00:58:33 +0300

    refresh/sort/search

--- TestWork/Controllers/HomeController.cs
+++ TestWork/Controllers/HomeController.cs
@@ -21,22 +21,58 @@ namespace TestWork.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string searchString, string sortOrder)
         {
-            return View(await _context.Advertisements.ToListAsync());
+           
+            var advertisementContext = _context.Advertisements.AsQueryable();
+            ViewData["LikeSortParm"] = String.IsNullOrEmpty(sortOrder) ? "like_desc" : "";
+            ViewData["DislikeSortParm"] = String.IsNullOrEmpty(sortOrder) ? "dislike_desc" : "dislike_order";
+            //  var students = from s in _context.AdvMark
+            //            select s;
+            switch (sortOrder)
+            {
+                case "like_desc":
+                    advertisementContext = advertisementContext.OrderByDescending(s => s.Likes);
+                    break;
+                case "dislike_desc":
+                    advertisementContext = advertisementContext.OrderByDescending(s => s.Dislikes);
+                    break;
+                case "dislike_order":
+                    advertisementContext = advertisementContext.OrderBy(s => s.Dislikes);
+                    break;
+                default:
+                    advertisementContext = advertisementContext.OrderBy(s => s.Likes);
+                    break;
+            }
+            ViewData["AdvFilter"] = searchString;
+            advertisementContext = from s in _context.Advertisements
+                                select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                advertisementContext = advertisementContext.Where(s => s.Company.Contains(searchString));
+            }
+            return View(await advertisementContext.ToListAsync());
         }
 
         
-        public async Task<List<Advertisement>> Edit(int id, bool like)
+        public async Task<List<Advertisement>> Edit(int id, bool like, bool dislike)
         {
             var advertisement = await _context.Advertisements.FindAsync(id);
             if (like)
             {
                 advertisement.Likes++;
             }
-            else
+           else if (!like)
+            {
+                advertisement.Likes--;
+            }
+            else if(dislike)
             {
                 advertisement.Dislikes++;
+            }
+            else
+            {
+                advertisement.Dislikes--;
             }
             _context.Update(advertisement);
             await _context.SaveChangesAsync();
